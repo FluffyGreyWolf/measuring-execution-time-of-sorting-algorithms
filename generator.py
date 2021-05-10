eb192bad3200e069fd9d75b9144010058ef9815e
GreyWolf <75756753+FluffyGreyWolf@users.noreply.github.com>
2021-05-10 20:06:47 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -95,41 +95,60 @@ def generaterandom():
 
 generaterandom()
 
+
 # generowanie posortowanych rosnąco liczb
-for a in ascendinglisty:
-    x = range(0, zakresy[ascendinglisty.index(a)])
-    for i in x:
-        a.append(i)
+def generateascending():
+    for a in ascendinglisty:
+        x = range(0, zakresy[ascendinglisty.index(a)])
+        for i in x:
+            a.append(i)
+
+generateascending()
 
 # generowanie posortowanych malejąco liczb
-for a in descendinglisty:
-    x = range(zakresy[descendinglisty.index(a)], 0, -1)
-    for i in x:
-        a.append(i)
+def generatedescending():
+    for a in descendinglisty:
+        x = range(zakresy[descendinglisty.index(a)], 0, -1)
+        for i in x:
+            a.append(i)
+
+generatedescending()
 
 # generowanie V-kształtnych liczb
-for a in vlisty:
-    x = range(zakresy[vlisty.index(a)]//2, 0 , -1)
-    y = range(1, zakresy[vlisty.index(a)]//2+1)
-    for i in x:
-        a.append(i)
-    for i in y:
-        a.append(i)
+def generatev():
+    for a in vlisty:
+        x = range(zakresy[vlisty.index(a)]//2, 0 , -1)
+        y = range(1, zakresy[vlisty.index(a)]//2+1)
+        for i in x:
+            a.append(i)
+        for i in y:
+            a.append(i)
+
+generatev()
 
 # generowanie A-kształtnych liczb
-for a in alisty:
-    x = range(zakresy[alisty.index(a)]//2, 0 , -1)
-    y = range(1, zakresy[alisty.index(a)]//2+1)
-    for i in y:
-        a.append(i)
-    for i in x:
-        a.append(i)
+def generatea():
+    for a in alisty:
+        x = range(zakresy[alisty.index(a)]//2, 0 , -1)
+        y = range(1, zakresy[alisty.index(a)]//2+1)
+        for i in y:
+            a.append(i)
+        for i in x:
+            a.append(i)
 
-# generowanie stałych liczb ( ta funkcja nie zostanie wywołana ponownie aż do zakończenia programu - tworzymy dane stałe )
-for a in constlisty:
-    for i in range(0, zakresy[constlisty.index(a)]):
-        x = random.randint(0, 20000)
-        a.append(x)
+generatea()
+
+# generowanie stałych liczb ( zawsze będą to te same losowe liczby )
+seed = 1
+def generateconst():
+    for a in constlisty:
+        for i in range(0, zakresy[constlisty.index(a)]):
+            random.seed(seed + i)
+            x = random.randint(0, 20000)
+            a.append(x)
+
+generateconst()
+
 
 """
 print("SELECTION SORT")
@@ -185,7 +204,7 @@ for a in vlisty:
     print("V-ksztaltne", len(a) , datetime.now() - startTime)
 print("")
 
-# selection sort (A=kształtne)
+# selection sort (A-kształtne)
 for a in alisty:
     startTime = datetime.now()
     for i in range(len(a)):
@@ -212,20 +231,169 @@ for a in constlisty:
 print("")
 """
 
+
 print("INSERTION SORT")
 
-for a in randomlisty:
-    for i in range(1, len(a)):
+random2000.clear()
+random4000.clear()
+random6000.clear()
+random8000.clear()
+random10000.clear()
+random12000.clear()
+random14000.clear()
+random16000.clear()
+random18000.clear()
+random20000.clear()
 
+ascending2000.clear()
+ascending4000.clear()
+ascending6000.clear()
+ascending8000.clear()
+ascending10000.clear()
+ascending12000.clear()
+ascending14000.clear()
+ascending16000.clear()
+ascending18000.clear()
+ascending20000.clear()
+
+descending2000.clear()
+descending4000.clear()
+descending6000.clear()
+descending8000.clear()
+descending10000.clear()
+descending12000.clear()
+descending14000.clear()
+descending16000.clear()
+descending18000.clear()
+descending20000.clear()
+
+v2000.clear()
+v4000.clear()
+v6000.clear()
+v8000.clear()
+v10000.clear()
+v12000.clear()
+v14000.clear()
+v16000.clear()
+v18000.clear()
+v20000.clear()
+
+a2000.clear()
+a4000.clear()
+a6000.clear()
+a8000.clear()
+a10000.clear()
+a12000.clear()
+a14000.clear()
+a16000.clear()
+a18000.clear()
+a20000.clear()
+
+const2000.clear()
+const4000.clear()
+const6000.clear()
+const8000.clear()
+const10000.clear()
+const12000.clear()
+const14000.clear()
+const16000.clear()
+const18000.clear()
+const20000.clear()
+
+generaterandom()
+generateascending()
+generatedescending()
+generatev()
+generatea()
+generateconst()
+
+# insertion sort (losowe dane)
+for a in randomlisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
         key = a[i]
 
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
-        j = i-1
-        while j >=0 and key < a[j] :
-                a[j+1] = a[j]
-                j -= 1
-        a[j+1] = key
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
 
-print(random2000)
+    print("Losowe", len(a) , datetime.now() - startTime)
+
+# insertion sort (posortowane rosnąco dane)
+for a in ascendinglisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
+        key = a[i]
+
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
+
+    print("Rosnace", len(a) , datetime.now() - startTime)
+
+# insertion sort (posortowane malejaco dane)
+for a in descendinglisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
+        key = a[i]
+
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
+
+    print("Malejace", len(a) , datetime.now() - startTime)
+
+# insertion sort (V-kształtne)
+for a in vlisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
+        key = a[i]
+
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
+
+    print("V-ksztaltne", len(a) , datetime.now() - startTime)
+
+# insertion sort (A-kształtne)
+for a in alisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
+        key = a[i]
+
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
+
+    print("A-ksztaltne", len(a) , datetime.now() - startTime)
+
+# insertion sort (dane stałe)
+for a in constlisty:
+    startTime = datetime.now()
+    for i in range(1, len(a)):
+        
+        key = a[i]
+
+        x = i-1
+        while x >= 0 and key < a[x] :
+                a[x+1] = a[x]
+                x -= 1
+        a[x+1] = key
+
+    print("Stale", len(a) , datetime.now() - startTime)
+
